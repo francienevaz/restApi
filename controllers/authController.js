7ab56615dbec27a9e3a782c53106d00bc446e8fa
francienevaz <fran.vfraga@gmail.com>
2024-10-24 14:41:43 -0300

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,13 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
     const token = req.header('authorization-token');
-    if (!token) return res.status(401).send("Access Denied") 
-    if (token) return res.send("token recebido!")
+    if (!token) return res.status(401).send("Access Denied"); 
+
+    try {
+        const userVerified = jwt.verify(token, process.env.SECRET);
+        req.user = userVerified;
+        next();
+    } catch (error) {
+        return res.status(401).send("Access Denied");
+    }
 }
